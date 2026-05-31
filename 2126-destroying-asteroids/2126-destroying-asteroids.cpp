2692d9a22de22ad121a027ac0db0c87740b1ec70
Muskan-076 <muskangupta252006@gmail.com>
2026-05-31 20:39:48 +0530

    Time: 39 ms (24.82%), Space: 106.7 MB (48.03%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+
+        long long currMass = mass;
+        for(int i =0; i< asteroids.size(); i++){
+            if(asteroids[i] <= currMass){
+                currMass += asteroids[i];
+                
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
