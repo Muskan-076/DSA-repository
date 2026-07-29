ccb776f6b1622f9578564a5194bd1aa14dfeafc5
Muskan <muskangupta252006@gmail.com>
2026-07-30 02:01:33 +0530

    Time: 10 ms (15.99%), Space: 17 MB (96.51%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,29 @@
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        const int INF = 1e9;
+
+        vector<int> dist(n, INF);
+        dist[src] = 0;
+
+        // Relax all edges k+1 times
+        for (int i = 0; i <= k; i++) {
+            vector<int> temp = dist;   // Copy previous distances
+
+            for (auto &flight : flights) {
+                int u = flight[0];
+                int v = flight[1];
+                int price = flight[2];
+
+                if (dist[u] != INF) {
+                    temp[v] = min(temp[v], dist[u] + price);
+                }
+            }
+
+            dist = temp;
+        }
+
+        return dist[dst] == INF ? -1 : dist[dst];
+    }
+};
